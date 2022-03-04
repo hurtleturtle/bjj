5e1c5221c7f9fe3e919d87c3f63d66ea0f37f0fd
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2022-03-04 13:43:58 +0000

    Update for attendance table

---
+++ scripts/update_attendance.sql
@@ -0,0 +1,14 @@
+SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE attendance;
+CREATE TABLE attendance (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    user_id INTEGER NOT NULL,
+    date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    class_id INTEGER NOT NULL,
+    class_date DATE NOT NULL,
+    class_time TIME NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (class_id) REFERENCES classes (id)
+);
+INSERT INTO attendance SELECT * FROM attendance_old;
+SET FOREIGN_KEY_CHECKS=1;
