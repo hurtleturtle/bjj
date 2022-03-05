c2ee260b504a1b9a03e319c224ffc0fc31a2a14f
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2022-03-05 01:42:18 +0000

    Included membership_type in attendance

--- nexusbjj/db.py
+++ nexusbjj/db.py
@@ -177,8 +177,9 @@ class Database:
 
     def get_attendance(self, from_date='', to_date='', user_id=None, class_id=None):
         query = 'SELECT classes.class_name, class_date, DATE_FORMAT(class_time, "%H:%i") class_time, '
-        query += 'CONCAT(users.first_name, " ", users.last_name) AS full_name, date check_in_time FROM attendance '
-        query += 'INNER JOIN classes ON attendance.class_id=classes.id INNER JOIN users ON attendance.user_id=users.id'
+        query += 'CONCAT(users.first_name, " ", users.last_name) AS full_name, membership_type, date check_in_time FROM attendance '
+        query += 'INNER JOIN classes ON attendance.class_id=classes.id INNER JOIN users ON attendance.user_id=users.id ' 
+        query += 'INNER JOIN memberships ON memberships.id=users.membership_id'
         where_clause = ' WHERE '
         conditions = []
         params = []
