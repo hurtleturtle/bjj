e97c502b7c1f1f4a55cbe30000ed04801091318e
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2022-03-17 15:16:09 +0000

    Added debug messages to emails

--- nexusbjj/email.py
+++ nexusbjj/email.py
@@ -4,7 +4,8 @@ from flask import current_app
 
 
 class Email:
-    def __init__(self, to='', text_body='', html_body='') -> None:
+    def __init__(self, to='', text_body='', html_body='', debug=False) -> None:
+        self.debug = debug
         self.msg = email.message.EmailMessage()
         self.domain = 'mail.warwickjudo.com'
         self.user = current_app.config['SMTP_USER']
@@ -14,9 +15,17 @@ class Email:
         self.msg['Subject'] = 'Password Reset for Warwick Judo & BJJ'
         self.msg.add_alternative(text_body)
         self.msg.add_alternative(html_body, subtype='html')
+
+        if debug:
+            print(f'msg: {self.msg}\n\nUser: {self.user}\nPass: {self.password}\n')
+
         
     def send_message(self):
         session = smtplib.SMTP('email-smtp.eu-west-2.amazonaws.com')
+
+        if self.debug:
+            session.set_debuglevel(2)
+
         session.ehlo()
         session.starttls()
         session.login(self.user, self.password)
