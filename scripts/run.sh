ec893935b8d1a38dd8b86139737caa773b897cb1
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2022-03-17 13:51:22 +0000

    Updated log file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 WEBAPP_HOME=$(dirname $(dirname $(readlink -f "$0")))
-LOG_FILE="<log_file>"
+LOG_FILE="$WEBAPP_HOME/instance/logs/webapp.log"
 source $WEBAPP_HOME/venv/bin/activate
 pkill uwsgi
 nohup uwsgi --socket 0.0.0.0:5000 --wsgi-file "$WEBAPP_HOME/wsgi.py" --logto $LOG_FILE
