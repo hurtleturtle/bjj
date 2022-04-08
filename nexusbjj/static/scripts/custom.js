6b7d640ed73cbba8153f5531b71d4d019ef7f0a0
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2022-04-09 00:34:04 +0100

    Added expand/collapse all

--- nexusbjj/static/scripts/custom.js
+++ nexusbjj/static/scripts/custom.js
@@ -16,3 +16,41 @@ function toggleAngle(element) {
     icon.classList.toggle('fa-chevron-right')
     icon.classList.toggle('fa-chevron-down')
 }
+
+function expand(anchors, content_divs) {
+    anchor_elements = document.querySelectorAll(anchors)
+    content_elements = document.querySelectorAll(content_divs)
+    i = 0
+
+    anchor_elements.forEach(element => {
+        element.classList.remove("collapsed")
+        element.setAttribute("aria-expanded", "true")
+
+        content = content_elements[i]
+        content.classList.add("show")
+        
+        icon = element.lastElementChild
+        icon.classList.add('fa-chevron-down')
+        icon.classList.remove('fa-chevron-right')
+        i += 1
+    });
+}
+
+function collapse(anchors, content_divs) {
+    anchor_elements = document.querySelectorAll(anchors)
+    content_elements = document.querySelectorAll(content_divs)
+    i = 0
+
+    anchor_elements.forEach(element => {
+        element.classList.add("collapsed")
+        element.setAttribute("aria-expanded", "false")
+
+        content = content_elements[i]
+        content.classList.remove("show")
+        
+        icon = element.lastElementChild
+        icon.classList.remove('fa-chevron-down')
+        icon.classList.add('fa-chevron-right')
+        i += 1
+    });
+}
