1764cd9cbd4e2af00b732a03e67eb3ee7f5e056b
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2022-04-09 00:06:15 +0100

    Add children

--- scripts/children.sql
+++ scripts/children.sql
@@ -8,3 +8,16 @@ CREATE TABLE children (
 
 ALTER TABLE attendance ADD COLUMN child_id INT DEFAULT NULL;
 ALTER TABLE attendance ADD constraint foreign key (child_id) REFERENCES children (id);
+
+-- CREATE TABLE child_attendance (
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+--     child_id INT NOT NULL,
+--     date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--     class_id INT NOT NULL,
+--     class_date DATE NOT NULL,
+--     class_time TIME NOT NULL
+-- );
+UPDATE classes SET age_group_id=2 WHERE id=19;
+SELECT * FROM attendance WHERE user_id=13 ORDER BY id DESC;
+DELETE FROM attendance WHERE user_id = 13 AND child_id;
+DELETE FROM children WHERE parent_id=13;
