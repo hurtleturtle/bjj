7dfa484ffd958dd1a1aa6e64cff7ac35bc3e2bd6
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2022-06-14 11:41:28 +0100

    Added get_remaining_classes

--- nexusbjj/api/attendance.py
+++ nexusbjj/api/attendance.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, g, request
-from nexusbjj.db import get_db
-from datetime import datetime
+from nexusbjj.db import get_db, QueryResult
+from datetime import datetime, timedelta
+import json
 
 bp = Blueprint('attendance', __name__, url_prefix='/api/attendance')
 
@@ -13,7 +14,18 @@ def confirm_check_in():
     return db.confirm_check_in(attendance_record_id, coach_id)
 
 
-def get_remaining_classes(user_id):
+@bp.route('/classes-remaining')
+def get_remaining_classes():
+    user_id = request.args.get('user_id')
     db = get_db()
     today = datetime.today()
-    total_attendance = db.get_attendance(from_date=today.replace(day=1), to_date=today, columns='*')
+    beginning_of_week = today - timedelta(days=today.weekday())
+    total_attendance = db.get_attendance(from_date=beginning_of_week, to_date=today, user_id=user_id)
+    membership = db.get_membership(user_id=user_id)
+    
+    result = {
+        'membership': membership,
+        'total_attendance': json.loads(QueryResult(total_attendance).to_json())
+    }
+
+    return result
